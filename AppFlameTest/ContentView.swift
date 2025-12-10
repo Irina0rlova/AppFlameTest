865e531f7524e4423679b09693bc8a687f47e5d4
Irina0rlova <irine.palamarchuk@gmail.com>
2025-12-10 16:06:45 +0200

    Initial commit

---
+++ AppFlameTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppFlameTest
+//
+//  Created by Iryna Orlova on 10.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
